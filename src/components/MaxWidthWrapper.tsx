2eff73d5b8aa96825cc9e341f4fe6573b96daf2a
Yann/Yaming <57467587+yamnmao@users.noreply.github.com>
2024-06-09 23:07:15 -0700

    update :wq

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,7 +1,19 @@
+import { ReactNode } from "react"
 import { cn } from "../lib/utils"
-
-export const MaxWidthWrapper=()=> {
-    return <div className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20")}>
-
+//{}:{type for typescript}, ?: means optional, doesn't need to exist, if does, then here is the type
+export const MaxWidthWrapper=({
+    className,
+    children
+}:{
+    className?:string,
+    children:ReactNode
+})=> {
+    //cn(....),we can pass arguments here
+    //md:px-20, tailwind is mobile first
+    return <div className={cn(
+        "h-full w-full max-w-screen-xl mx-auto px-2.5 md:px-20",
+        className
+        )}>
+            {children}
     </div>
 }
