08aee1ec85da8fe9138a58ad88cbbdfbeb0a8543
Yann/Yaming <57467587+yamnmao@users.noreply.github.com>
2024-06-11 15:29:50 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { MaxWidthWrapper } from "../components/MaxWidthWrapper";
 import { ArrowRight, Check, Star } from 'lucide-react'
 import Image from "next/image";
+import Phone from "../components/Phone";
 
 export default function Home() {
   return (
@@ -89,9 +90,24 @@ export default function Home() {
               </div>
 
               <p>
-                    <span className='font-semibold'>1.250</span> happy customers
-                  </p>
+                  <span className='font-semibold'>1.250</span> happy customers
+              </p>
+            </div>
+          </div>
 
+          <div className='col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0
+          mt-32 lg:mx-0 lg:mt-20 h-fit'>
+            <div className="relative md:max-w-xl">
+              <img 
+                src="/your-image.png" 
+                className = 'absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block'
+              />
+              <img
+                src='/line.png'
+                className='absolute w-20 -left-6 -bottom-6 select-none'
+              />
+              <Phone className='w-64' imgSrc='/testimonials/1.jpg' />
+            
             </div>
           </div>
         </MaxWidthWrapper>
---
+++ src/components/Phone.tsx
@@ -0,0 +1,17 @@
+
+import { cn } from "../lib/utils"
+
+interface PhoneProps extends HTMLAttributes<HTMLDivElement>{
+    imgSrc: string
+    dark?:boolean
+}
+
+const Phone = ({imgSrc,className,dark=false,...props}:PhoneProps) => {
+    return (
+        <div className={
+            cn("relative pointer-events-none z-50 overflow-hidden",
+            className)}>
+        </div>
+    )
+}
+export default Phone
